935797e575266c52d0274a2481dd146a90912df2
AlliesChen <cyphuaenng@pm.me>
2021-11-05 23:51:19 +0800

    complete the descending-color version

--- js-file.js
+++ js-file.js
@@ -1,14 +1,19 @@
 const container = document.querySelector('#container');
+let primeColor = 0; // for realizing 10% darken in 10 steps
 addCells();
 
 // Use event listener on the clearCanvas button
 const resetButton = document.querySelector('#resetButton');
+
 resetButton.addEventListener('click', () => {
     removeCells();
     // Base on the number, to create a num*num grids plate. 
     setPlate(getGridNum());
 });
 
+// the functions
+
+// Assign the grid box corresponded to user's input
 function setPlate(gridNum) {
     container.style.cssText = `grid-template-rows: repeat(${gridNum}, 1fr); grid-template-columns: repeat(${gridNum}, 1fr)`;
     addCells(gridNum);
@@ -20,7 +25,6 @@ function getGridNum () {
     let grids = Number(userInput);
 
     if (!Number.isInteger(grids) || grids < 1 || grids > 100) getGridNum()
-
     return grids;
 }
 
@@ -35,21 +39,69 @@ function removeCells() {
     // querySelectorAll won't work on the way mentioned above due to its a static list
 }
 
+function darkenColor(currentColors) {
+    // start from the highest value in the array
+    let highestVal = maxValue(currentColors);
+    let newHighestVal = Math.round(highestVal - Math.min(primeColor * 0.1, highestVal));
+    // decreasing the other values by the same fraction
+    let decreaseFrac = (highestVal - newHighestVal) / highestVal;
+
+    for (let i = 0; i < 3; i++) {
+        if (currentColors[i] == highestVal) {
+            currentColors[i] = newHighestVal;
+        } else {
+            currentColors[i] = Math.round(currentColors[i] * (1 - decreaseFrac));
+        }
+    }
+    return(`rgb(${currentColors.join(',')})`);
+}
+
+// just see a comment says this got a better run-time than Math.max
+function maxValue(arr) {
+    let max = arr[0];
+  
+    for (let val of arr) {
+      if (val > max) max = val;
+    }
+    return max;
+}
+
+// use for making random RGB colors
 function getRandom(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function createColorSet(valueArray) {
+    // if the color hasn't been set, make one
+    if (!valueArray.length) {
+
+        for (let i = 0; i < 3; i++) {
+            valueArray[i] = getRandom(1, 255);
+        }
+        primeColor = maxValue(valueArray);
+        return(`rgb(${valueArray.join(',')})`);
+
+    } else { // 10% darken the color
+        return darkenColor(valueArray);
+    }
+}
+
 function colorMouseover() {
     const cells = document.querySelectorAll('#container > div');
+    let rgbValue = [];
+
     cells.forEach(cell => {
         cell.addEventListener('mouseover', () => {
-            cell.style.backgroundColor = `rgb(${getRandom(0, 255)}, ${getRandom(0, 255)}, ${getRandom(0, 255)})`;
+            cell.style.backgroundColor = `${createColorSet(rgbValue)}`;
+            console.log(`rgb(${rgbValue.join(',')})`);
         });
     });
 }
 
 function addCells(gridNum) {
+
     let i = (gridNum*gridNum) || (16 * 16);
+
     for (;i > 0; i--) {
         const cell = document.createElement('div');
         // The stylesheet draws borders base on class
